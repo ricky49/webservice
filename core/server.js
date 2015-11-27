0061d37d4a95f8b181a10df81ec49f347a05e96d
Ricky Soto <rickysotosanchezz@gmail.com>
2015-11-26 21:16:24 -0400

    ponerle puerto 2000 de nuevo

--- core/server.js
+++ core/server.js
@@ -8,32 +8,31 @@
 var express=require('express');
 var mongoose=require('mongoose');
 var bodyparser=require('body-parser');
-var cors=require('cors');
-//var methodOverride = require('method-override');
+var morgan = require('morgan');
+
 
 //express
 var app=express();
-//app.use(cors);
+
 app.use(bodyparser.urlencoded({
     extended: true
 }));
 
 app.use(bodyparser.json);
 
+//app.use(morgan('dev'));
 //app.use(methodOverride());
 
 //MongoDB
 mongoose.connect('mongodb://localhost/darr');
 
 //Routes
+
 app.use('/api', require('../routes/api'));
 
 //start server
 app.listen(2000);
 console.log('server is running at port 2000');
-
-
-
 
 
 
